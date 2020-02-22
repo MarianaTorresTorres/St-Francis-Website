e142d1dbb41e6c693eec08e2f5908ae0b65d9b37
Fernanda Campos <fcampos1299@gmail.com>
2020-02-22 18:44:42 -0500

    AboutUs

--- client/src/pages/AboutUs.js
+++ client/src/pages/AboutUs.js
@@ -1,7 +1,37 @@
 import React from 'react';
 function AboutUs(){
-  return ()
-    <p> Hello World </p>
+  return (
+    <>
+    <h2> Mission</h2>
+    <p> Our mission is to empower families with children to transition from homelessness to self-sufficiency by providing case management, housing, food, training and educational resources in a secure enviroment.</p>
+    <h2> History</h2>
+    <p>St. Francis House was established in 1980 by Father Baker. His goal was to provide the necessities to help the impoverished and hungry in his community</p>
+    <h2>The Structure of St. Francis House </h2>
+    <p> The current Board has 10 members, some of whom have been on the board for 15+ years. </p>
 
-
+    <h2> What does St. Francis House Provide? </h2>
+    <ul>
+    <li> Temporary Emergency Shelter for Families with Children: </li>
+      <ul>
+        <li>  St. Francis House houses 35 - 40 shelter guests, with preference given to families</li>
+        <li> Typically, guests stay for an average of 2-4 weeks. </li>
+      </ul>
+    <li> Case management:</li>
+      <ul>
+        <li>  St. Francis House has two case managers on site, who also oversee our two other properties.</li>
+        <li>  Guests are required to meet regularly with the case manager and set up individualized case plans.</li>
+        <li>  Case managers refer guests to local community resources that best meet their immediate needs.</li>
+        <li>  Homelessness is a complex social problem - more than 60% of homeless adults reported being homeless at least one or more times as a child. St Francis House is trying to break this cycle by teaching self-sufficiency.</li>
+        <li>    On average, 33% of employable residents find jobs before or upon discharge and 25% have jobs when they became residents.</li>
+     </ul>
+    <li>Meal Service:</li>
+      <ul>
+        <li>  Breakfast is served to shelter residents from 6.00a - 7.00a. </li>
+        <li>  Lunch is served from 9.00a-12.00p every day. Lunch is provided to anyone who needs it that has police clearance, not just shelter residents.</li>
+        <li> Dinner is usually provided by service groups or community organizations (for shelter resident only) starting at 6.00p every night.</li>
+      </ul>
+      </ul>
+    </>
+  )
   }
+export default AboutUs;
