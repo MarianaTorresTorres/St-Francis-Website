979365880f17168ed1aa1da564f9a476de95f81c
Valentina Casteline <valentinacasteline@Valentinas-MacBook-Pro-2.local>
2020-02-22 20:31:54 -0500

    f

--- client/src/pages/Navbar.js
+++ client/src/pages/Navbar.js
@@ -4,32 +4,13 @@ import './Navbar.css';
 function Navbar(){
     return(
         <div class = "menu-bar">
-<<<<<<< HEAD
-
-        <img id="imageLogo" src="https://i.imgur.com/h3g0P6f.png"></img>
-
-        <a href="#volunteering">Volunteering </a>
-        <a href="#staff">Staff </a>
-        <a href="#contact-us">Contact Us </a>
-        <a href="#about-us">About Us </a>
-        <a href="#home">Home </a>
-
-        <h1>St Francis House, Inc</h1>
-
-      </div>
-
-    );
-}
-
-export default Navbar;
-=======
         
         <img id="imageLogo" src="https://i.imgur.com/h3g0P6f.png"></img>
        
         <a href="http://localhost:3000/volunteering">Volunteering </a>
         <a href="http://localhost:3000/staff">Staff </a>
         <a href="http://localhost:3000/contact">Contact Us </a>
-        <a href="#about-us">About Us </a>
+        <a href="http://localhost:3000/about">About Us </a>
         <a href="#home">Home </a>
         
         <p>St Francis House, Inc</p>
@@ -40,4 +21,3 @@ export default Navbar;
 }
 
 export default Navbar;
->>>>>>> 1dd1bb43639ab5d9c114b44d3bc07cbcf396989f
