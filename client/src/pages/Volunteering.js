f7835e16116c03c8f20f53f02d47bef0c531b100
MarianaTorresTorres <mari.torret@gmail.com>
2020-02-22 20:07:41 -0500

    finished volunteering and made staff page

--- client/src/pages/Volunteering.js
+++ client/src/pages/Volunteering.js
@@ -2,6 +2,11 @@ import React from "react";
 import { Form, Checkbox, Grid, Container } from "semantic-ui-react";
 import './Volunteering.css';
 
+const options = [
+  { key: 'm', text: 'Male', value: 'male' },
+  { key: 'f', text: 'Female', value: 'female' },
+  { key: 'x', text: 'Prefet not to say', value: 'no' },
+]
 const Volunteering = () => (
   <div id = "volunteer">
     <Grid style={{padding: '4vw'}}>
@@ -14,6 +19,12 @@ const Volunteering = () => (
           <Form.Group widths="equal">
             <Form.Input fluid label="First name" placeholder="First name" />
             <Form.Input fluid label="Last name" placeholder="Last name" />
+            <Form.Select
+            fluid
+            label='Gender'
+            options={options}
+            placeholder='Gender'
+            />
           </Form.Group>
           <Form.Group widths="equal">
             <Form.Input
@@ -40,6 +51,30 @@ const Volunteering = () => (
             />
           </Form.Group>
           <Checkbox label="I am over 18" />
+          <Form.Input
+            fluid
+            label="Best form of contact"
+            placeholder="..."
+          />
+          <Form.Input
+            fluid
+            label="How did you learn about our Volunteering Program?"
+            placeholder="Tell us here"
+          />
+          <Form.Group>
+            <Checkbox label="I am interested in long-term involvement" />
+            <Checkbox label="I am interested in short-term involvement" />
+          </Form.Group>
+          <Form.Group>
+            <Checkbox label="I am receiving class credit for volunteering hours served" />
+            <Checkbox label="I am receiving credit at work for volunteering hours served" />
+          </Form.Group>
+          <Form.Input
+            fluid
+            label="Is there a specific volunteer opportunity that you are interested in ?"
+            placeholder="Tell us here"
+          />
+          <Form.Button>Submit</Form.Button>
         </Form>
       </Grid.Row>
     </Grid>
