113f6e2b4baeebe52dd46d21e1f4f9ed84ba782e
isamitre <isamitre@gmail.com>
2020-02-22 19:57:34 -0500

    default email exists

--- client/src/Email.js
+++ client/src/Email.js
@@ -9,18 +9,25 @@ let transport = nodemailer.createTransport({
      }
 });
 
-const email =
-const message = {
-    from: 'isamitre@gmail.com', // Sender address
-    to: '40ec1310ca-3ab0e0@inbox.mailtrap.io',         // List of recipients
-    subject: 'Volunteer Form', // Subject line
-    text: 'info here?' // Plain text body
-};
+const recipient ='40ec1310ca-3ab0e0@inbox.mailtrap.io';
 
-exports.sendMail = transport.sendMail(message, function(err, info) {
-    if (err) {
-      console.log(err)
-    } else {
-      console.log(info);
-    }
-});
+// const message = {
+//     from: 'isamitre@gmail.com', // Sender address
+//     to: recipient,         // List of recipients
+//     subject: 'Volunteer Form', // Subject line
+//     text: 'info here' // Plain text body
+// };
+
+// message will have from and text
+exports.sendMail = (message) => {
+  message['to'] = recipient
+  message['subject'] = 'Volunteer Form'
+
+  transport.sendMail(message, function(err, info) {
+      if (err) {
+        console.log(err)
+      } else {
+        console.log(info);
+      }
+  });
+}
